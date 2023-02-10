c51af9bd41254ae1386508772415b2ed0458a905
Vyshu007 <yellamanchivyshnavi07@gmail.com>
2023-02-10 19:27:45 +0530

    sol

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {Bar} from 'react-chartjs-2'; 
+import { Chart,LinearScale,CategoryScale,BarElement,Legend,Title ,Tooltip} from 'chart.js'; 
+Chart.register(
+  LinearScale,CategoryScale,BarElement,Legend,Title ,Tooltip
+)
 
+const labels = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'] 
+const options ={
+  plugins:{ 
+    legend:{
+      position:'top'
+    },
+    title:{ 
+      display:true,
+      text:'Expensive Tracker'
+    }
+  }
+}
+const data = {
+  labels,
+  datasets : [
+    {
+      label:'2020 expense',
+      data:[10000,9000,8000,10200,10500,8400,13000,8900,14600,17000,9000,12500],
+      backgroundColor:'pink'
+    },
+    {
+      label:'2021 expense',
+      data:[17000,11000,12500,11000,12100,7500,13800,8100,9600,10000,11000,8000],
+      backgroundColor:'grey'
+    }
+  ]
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>  
+    <div style={{'height':400,'width':800,'margin':'auto'}}>
+    <Bar options={options} data={data}/> 
     </div>
+    </> 
   );
 }
 
